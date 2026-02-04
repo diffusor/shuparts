a2a528282efa7a4a51d1a74b0028e489d9f79f98
Steven Mueller <1826455+diffusor@users.noreply.github.com>
2026-02-04 00:02:59 -0700

    Support sections and part descriptions
    When a musescore file is split into multiple sections, number them
    and let the user select which section to practice.
    
    Also, if the file contains [part descriptions] (Staff Text surrounded
    by square brackets), append them to each part name, along with info
    about how many times they've been repeated in the file and how many parts
    are associated with each description.

--- shuparts.py
+++ shuparts.py
@@ -6,9 +6,24 @@ import random
 import zipfile
 import xml.etree.ElementTree as ET
 
-def shuf(parts):
-    print("Selecting from parts:", ", ".join(parts))
-    print("<hit enter to pick the next part, or ctrl-C to exit early>")
+def shuf(partspec):
+    print("Available parts:\n")
+    parts = []
+    if partspec and isinstance(partspec[0], list):
+        for i, subparts in enumerate(partspec):
+            print(f"  section {i+1}: {' '.join(subparts)}")
+            parts.extend(f"{i+1}:{part}" for part in subparts)
+        section = input("\nEnter section to choose from, or hit enter to practice all: ")
+        if section:
+            i = int(section)
+            parts = list(partspec[i-1])
+
+    else:
+        parts.extend(partspec)
+        print(" ", " ".join(parts))
+
+    print("Selecting from parts:\n ", " ".join(parts))
+    print("\n<hit enter to pick the next part, or ctrl-C to exit early>")
 
     maxlen = max(map(len, parts))
 
@@ -35,6 +50,8 @@ def extract_parts_from_file(fname):
 
 def extract_parts_from_mscz(fname):
     """Parse all rehearsal marks from the given mscz file.
+
+    returns a list of sections, each section having a list of parts.
     """
     with zipfile.ZipFile(fname, 'r') as mscz:
         # Read the contents of the internal mscx file as bytes
@@ -55,11 +72,42 @@ def extract_parts_from_mscx(fname):
 def extract_parts_from_mscx_str(mscx_str):
     root = ET.fromstring(mscx_str)
     parts = []
-    for mark in root.iter('RehearsalMark'):
-        part_element = mark.find('text')
-        parts.append(part_element.text.strip())
+    sections = [parts] # list of lists
+    description = ""
+    desc_uses = {}
+    desc_subpart = 0
+    for measure in root.iter('Measure'):
+        # Find part description to append to each part until the next section
+        for staff_text in measure.iter('StaffText'):
+            text = staff_text.find('text').text.strip()
+            if text.startswith('['):
+                description = text.replace(" ", "-")
+                desc_uses.setdefault(description, 0)
+                desc_uses[description] += 1
+                desc_subpart = 0
 
-    return parts
+        for mark in measure.iter('RehearsalMark'):
+            part = mark.find('text').text.strip()
+            if description:
+                part = f"{part}{description}"
+                desc_usecount = desc_uses[description]
+                if desc_usecount > 1:
+                    part = f"{part}{desc_usecount}"
+
+                desc_subpart += 1
+                if desc_subpart > 1:
+                    part = f"{part}.{desc_subpart}"
+            parts.append(part)
+
+        for layout_break in measure.iter('LayoutBreak'):
+            subtype = layout_break.find('subtype')
+            if subtype is not None and subtype.text.strip() == 'section':
+                parts = []
+                sections.append(parts)
+                description = ""
+                desc_subpart = 0
+
+    return sections
 
 def extract_parts_from_mxl(fname):
     """Parse all rehearsal marks from the given compressed MusicXML mxl file.
@@ -88,10 +136,69 @@ def extract_parts_from_xml_str(xml_str):
 
     return parts
 
+def showhelp():
+    print(f"""Usage: {sys.argv[0]} partname ...
+
+  This is a flashcard-like tool for testing your memorization.
+
+  Given a list of part names, shuparts randomly shuffles them and prints them
+  one at a time, waiting for the user to hit enter between each.
+
+  Hit ctrl-C to exit early.  Otherwise, shuparts exits after each part has
+  been printed, like going through one pass of a deck of flashcards.
+
+Single-string support:
+
+  If only one argument is passed to shuparts, it will be split at spaces.
+  This makes it easy to copy-paste a space-separated list of parts from
+  elsewhere or from shuparts' own output.
+
+MuseScore Studio support:
+
+  If only one argument is given and it ends it .mscz or .mscx, it's
+  interpreted as a MuseScore file to parse.  The expected format is as follows:
+
+  * The contents of each rehearsal mark is used as the list of parts
+
+  * Section breaks are used to split up music files containing multiple pieces
+    of music.  Each section is presented as a separate group of parts to
+    practice.  When shuparts starts, it will query the user to select a section
+    if there are more than one.
+
+  * Staff text is used to provide descriptive names to each part.
+
+Music layout hints:
+
+  * Select a note in the top staff and use ctrl-M in MuseScore Studio to
+    insert a new rehearsal mark.
+
+  * Use section breaks to split up a single score into multiple sections.
+    This allows reusing the same rehearsal marks without mixing up the
+    list of parts.
+
+  * Add "staff text" on the top staff to provide part descriptions.
+    The staff text must be enclosed in [square brackets].
+    Note: If a measure contains both a rehearsal mark and a part description,
+    shuparts assumes the part description applies to that part, regardless of
+    which notes each is actually attached to within the measure.
+
+  * Only add rehearsal marks and [] staff text to the topmost staff -
+    otherwise the marks may get jumbled up with the descriptive text
+    and the section parsing.
+""")
+
+    # (This is because MuseScore stores music as lists of staffs, where each
+    # staff contains a list of measures.  Section breaks are only stored in the
+    # measures in the first staff.  To support putting rehearsal marks and
+    # descriptions on separate staves, shuparts would need to re-zip the music
+    # into a list of measures units at the top level, where each measure unit
+    # contains multiple staffs, each with it's own measure.)
+
+    # TODO - find a way to name the sections.
 
 def main():
-    if len(sys.argv) == 1:
-        print(f"Usage: {sys.argv[0]} partname ...")
+    if len(sys.argv) == 1 or sys.argv[1] in "-h --help /?".split():
+        showhelp()
         return(1)
 
     elif len(sys.argv) == 2:
